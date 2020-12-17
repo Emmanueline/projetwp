b0ffefa134c381e36dc42078b9325c8d40c025b5
Emmanueline <47985021+Emmanueline@users.noreply.github.com>
2020-12-17 20:40:00 +0100

    header updt

--- header.php
+++ header.php
@@ -19,51 +19,64 @@
     <?php wp_head(); ?>
 </head>
 
-<body>
-<div class="container">
-    <!-- Partie haute du site, logo...  -->
-    <header class="blog-header py-3 text-center">
-        <a class="blog-header-logo text-dark" href="<?php bloginfo('url') ?>">
+<body class="bg-light">
+<div class="container col-12">
+
+
+    <!-- navbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-white bg-white">
+        <a class="navbar-brand navbar-brand" href="<?php bloginfo('url') ?>">
             <?php if (has_custom_logo()) : ?>
                 <?php the_custom_logo(); ?>
             <?php else : ?>
                 <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
             <?php endif; ?></a>
-    </header>
-    <!-- Fin partie haute du site -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <!-- Menu -->
-
-    <div class="bg-white">
-        <nav class="container text-center navbar navbar-expand-lg navbar-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigations-02"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse justify-content-md-center" id="navigations-02">
-                <div class="row w-100">
-                    <div>
-                        <?php wp_nav_menu(array(
-                            'theme_location' => 'main',
-                            'container' => 'ul',
-                            'menu_class' => 'navbar-nav text-dark',
-                        ));
-                        ?>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto pull-sm-right">
+                <li class="nav-item pull-right">
+                    <a class="nav-link disabled" href="https://www.redlab.io/les-objets-connectes" tabindex="-1" aria-disabled="true">IOT</a>
+                </li>
+                <li class="nav-item pull-right">
+                    <a class="nav-link disabled" href="https://www.redlab.io/le-big-data" tabindex="-1" aria-disabled="true">Big Data</a>
+                </li>
+                <li class="nav-item pull-right">
+                    <a class="nav-link disabled" href="https://www.redlab.io/advanced-analytics" tabindex="-1" aria-disabled="true">Advanced Analytics</a>
+                </li>
+                <li class="nav-item pull-right">
+                    <a class="nav-link disabled" href="https://www.redlab.io/la-blockchain" tabindex="-1" aria-disabled="true">Blockchain</a>
+                </li>
+                <li class="nav-item pull-right">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">L'agence</a>
+                </li>
+                <li class="nav-item dropdown active pull-right">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Le blog
+                        <span class="sr-only">(current)</span></a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">IOT</a>
+                        <a class="dropdown-item" href="#">Big Data</a>
+                        <a class="dropdown-item" href="#">Advanced Analytics</a>
+                        <a class="dropdown-item" href="#">Blockchain</a>
                     </div>
-                </div>
-            </div>
-        </nav>
-    </div>
-    <div class="pb-4 mb-4"></div>
+                </li>
+                <li class="nav-item pull-right">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+                </li>
+            </ul>
 
-    <!-- Fin Menu -->
-</div>
-
-<!-- Hero Header -->
-<div class="container">
-    <div class="jumbotron p-4 p-md-5 text-dark rounded bg-light text-center">
-        <div class="col-md-6 px-0">
-            <h1 class="display-4 font-italic"><?php the_title(); ?></h1>
-            <p class="lead my-3">Découvrez notre super blog !</p>
-            <p class="lead mb-0"><a href="<?php the_permalink() ?>" class="text-dark font-weight-bold">En savoir plus...</a></p>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2 rounded-pill" type="search" placeholder="" aria-label="Rechercher">
+                <button class="btn btn-outline-danger rounded-pill px-4 my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-    </div>
+    </nav>
+
+    <!-- Hero -->
+    <header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png;"></header>
+
 </div>
-<!-- Fin Hero Header -->
