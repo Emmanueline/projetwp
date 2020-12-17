aa35c1c40569fbeea612f53ff57b83401f7f0733
Emmanueline <47985021+Emmanueline@users.noreply.github.com>
2020-12-17 20:47:43 +0100

    base footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="blog-footer text-dark">
-    <p>REDLab 2018. Tous droits réservés. Blog crée par <a href="#" class="text-dark">HTMELLES</a>.</p>
+    <p>REDLab 2018. Tous droits réservés. Blog créé par <a href="#" class="text-dark">HTMELLES</a>.</p>
     <p>
         <a href="#" class="text-dark">Mentions Légales</a>
         <a href="#" class="text-dark">Glossaire Technique</a>
