b68ce3ee4df52a8059246086befc7e469a12601b
Emmanueline <47985021+Emmanueline@users.noreply.github.com>
2020-12-17 20:34:45 +0100

    home page changements

--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <?php get_header();?>
 
 <!-- Contenu principal -->
-<main role="main" class="container">
+<main role="main" class="container col-12">
     <div class="row">
-        <div class="col-md-8 blog-main">
+        <div class="col-12 blog-main bg-light">
             <br>
-            <h3 class="pb-4 mb-4 font-italic border-bottom">En direct de notre blog...</h3>
 
-            <!-- Boucle WordPress pour la liste des articles -->
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <?php if (!is_sticky()) : ?>
-                    <div class="card bg-light" style="width: 45rem;">
-                        <div class="blog-post card-body">
-                            <h2 class="blog-post-title card-title"><?php the_title() ?></h2>
-                            <p class="blog-post-meta"><?php the_date(); ?> par <a href="#" class="text-dark"><?php the_author() ?></a></p>
-                            <hr class="my-4">
-                            <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-img-top', 'alt' =>'', 'style' => 'height: auto;']); ?>
-                            <div class="pb-4 mb-4"></div>
-                            <div class="lead">
-                                <?php the_excerpt() ?>
+            <div class="col-6">
+                <!-- Boucle WordPress pour la liste des articles -->
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php if (!is_sticky()) : ?>
+                        <div class="card bg-white border rounded-0" style="width: 30rem;">
+                            <div class="blog-post card-body">
+                                <h2 class="blog-post-title card-title"><?php the_title() ?></h2>
+                                <p class="blog-post-meta"><?php the_date(); ?> par <a href="#" class="text-dark"><?php the_author() ?></a><a href="#"><?php the_category() ?></a></p>
+                                <hr class="my-4">
+                                <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-img-top', 'alt' =>'', 'style' => 'height: auto;']); ?>
+                                <div class="pb-4 mb-4"></div>
+                                <div class="lead">
+                                    <?php the_excerpt() ?>
+                                </div>
+                                <div class="pb-4 mb-1"></div>
+                                <a class="btn btn-danger rounded-pill px-3" href="<?php the_permalink() ?>">Lire l'article complet </a>
                             </div>
-                            <div class="pb-4 mb-1"></div>
-                            <a class="btn btn-warning" href="<?php the_permalink() ?>"> Lire l'article complet </a>
                         </div>
-                    </div>
-                    <div class="pb-4 mb-4"></div>
+                        <div class="pb-4 mb-4"></div>
+                    <?php endif; ?>
+                <?php endwhile; ?>
                 <?php endif; ?>
-            <?php endwhile; ?>
-            <?php endif; ?>
+            </div>
             <!--Fin boucle WordPress -->
 
             <!-- Pagination -->
@@ -39,8 +40,6 @@
         </div>
 
         <!-- Fin contenu principal -->
-
-        <?php get_sidebar() ?>
 
     </div><!-- /.row -->
 </main><!-- /.container -->
--- page.php
+++ page.php
@@ -3,37 +3,43 @@
 <!-- Contenu principal -->
 <main role="main" class="container col-12">
     <div class="row">
-        <div class="col-md-8 blog-main">
+        <div class="col-12 blog-main bg-light">
             <br>
-            <h3 class="pb-4 mb-4 font-italic border-bottom">En direct de notre blog...</h3>
 
-            <!-- Boucle WordPress pour la liste des articles -->
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <?php if (!is_sticky()) : ?>
-                    <div class="blog-post">
-                        <h2 class="blog-post-title"><?php the_title() ?></h2>
-                        <p class="blog-post-meta"><?php the_date(); ?> par <a href="#"><?php the_author() ?></a></p>
-                        <?php the_post_thumbnail(); ?><br /><?php the_excerpt() ?>
-                        <a class="btn btn-primary bg-info" href="<?php the_permalink() ?>"> Lire l'article complet </a>
-                    </div>
+            <div class="col-6">
+                <!-- Boucle WordPress pour la liste des articles -->
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php if (!is_sticky()) : ?>
+                        <div class="card bg-white border rounded-0" style="width: 30rem;">
+                            <div class="blog-post card-body">
+                                <h2 class="blog-post-title card-title"><?php the_title() ?></h2>
+                                <p class="blog-post-meta"><?php the_date(); ?> par <a href="#" class="text-dark"><?php the_author() ?></a><a href="#"><?php the_category() ?></a></p>
+                                <hr class="my-4">
+                                <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-img-top', 'alt' =>'', 'style' => 'height: auto;']); ?>
+                                <div class="pb-4 mb-4"></div>
+                                <div class="lead">
+                                    <?php the_excerpt() ?>
+                                </div>
+                                <div class="pb-4 mb-1"></div>
+                                <a class="btn btn-danger rounded-pill px-3" href="<?php the_permalink() ?>">Lire l'article complet </a>
+                            </div>
+                        </div>
+                        <div class="pb-4 mb-4"></div>
+                    <?php endif; ?>
+                <?php endwhile; ?>
                 <?php endif; ?>
-            <?php endwhile;
-            else : ?>
-                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-            <?php endif; ?>
+            </div>
             <!--Fin boucle WordPress -->
 
             <!-- Pagination -->
-            <nav class="blog-pagination">
-                <?php previous_posts_link('Articles précédents'); ?>
-                <?php next_posts_link('Articles suivants'); ?>
+            <nav class="blog-pagination text-dark">
+                <?php previous_posts_link('Articles Précédents'); ?>
+                <?php next_posts_link('Articles Suivants'); ?>
             </nav>
 
         </div>
 
         <!-- Fin contenu principal -->
-
-        <?php get_sidebar() ?>
 
     </div><!-- /.row -->
 </main><!-- /.container -->
