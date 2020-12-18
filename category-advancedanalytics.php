f6ec3dede530ec7fa13656820123f52da3bf8ad6
Teddysa <emilie.lefebvre1999@gmail.com>
2020-12-18 10:56:28 +0100

    ajout footer category + banner index

--- category-advancedanalytics.php
+++ category-advancedanalytics.php
@@ -4,3 +4,8 @@
 <header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png">
     <p class="text-md-left">Left aligned text on viewports sized MD (medium) or wider.</p>
 </header>
+
+<?php get_footer();?>
+</body>
+
+</html>
--- category-blockchain.php
+++ category-blockchain.php
@@ -1,4 +1,11 @@
 <?php get_header();?>
 
 <!-- Hero -->
-<header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png"></header>
+<header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png">
+    <p class="text-md-left">Left aligned text on viewports sized MD (medium) or wider.</p>
+</header>
+
+<?php get_footer();?>
+</body>
+
+</html>
