1467841bb7bdf93a2125d489a90db89e15887ada
Emmanueline <47985021+Emmanueline@users.noreply.github.com>
2020-12-18 13:50:45 +0100

    changement bannière

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
 <!-- Hero -->
-<header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png">
+<header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/nouvelles-technologies-redlab.png'">
 </header>
 
 <!-- Contenu principal -->
