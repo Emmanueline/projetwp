b1a3f12aafaa18207d3d7a3dc7565148172cf304
Capucine Vandercoilden <66354359+Capucine-Vndcln@users.noreply.github.com>
2020-12-18 13:53:52 +0100

    Mise à jour des liens du footer

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 <footer class="blog-footer text-dark">
     <p>REDLab 2018. Tous droits réservés. Blog créé par <a href="#" class="text-dark">HTMELLES</a>.</p>
     <p>
-        <a href="#" class="text-dark">Mentions Légales</a>
-        <a href="#" class="text-dark">Glossaire Technique</a>
-        <a href="#" class="text-dark">Plan du site</a>
+        <a href="https://cvanderc.chef2projet.digital/www/wordpress/mentions-legales" class="text-dark">Mentions Légales</a>
+        <a href="https://www.redlab.io/glossaire-technique" class="text-dark">Glossaire Technique</a>
+        <a href="https://cvanderc.chef2projet.digital/www/wordpress/page-sitemap.xml" class="text-dark">Plan du site</a>
     </p>
     <?php wp_footer(); ?>
 </footer>
