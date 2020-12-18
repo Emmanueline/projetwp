2c6a1271edde64086e9166d53fa6fe982d6bb063
Capucine Vandercoilden <66354359+Capucine-Vndcln@users.noreply.github.com>
2020-12-18 14:18:21 +0100

    Mise à jour de alt

--- 404.php
+++ 404.php
@@ -6,20 +6,22 @@
 
             <div class="jumbotron jumbotron-fluid text-center">
 
-                <h1 class="entry-title"><?php _e( 'Page Introuvable', 'tpfinal' ); ?></h1>
+                <img src="https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/page-inexistante.jpg">
 
-                <div class="intro-text"><p><?php _e( 'La page que vous cherchez n\'a pas pu être trouvée. Elle a peut-être été déplacée, renommée ou supprimée.', 'tpfinal' ); ?></p></div>
+                <h1 class="entry-title"><?php _e( 'Page Introuvable', 'redlab' ); ?></h1>
+
+                <div class="intro-text"><p><?php _e( 'La page que vous cherchez n\'a pas pu être trouvée. Elle a peut-être été déplacée, renommée ou supprimée.', 'redlab' ); ?></p></div>
 
                 <?php
                 get_search_form(
                     array(
-                        'label' => __( '404 not found', 'tpfinal' ),
+                        'label' => __( '404 not found', 'redlab' ),
                     )
                 );
                 ?>
 
                 <div class="col-md-4 mx-auto">
-                    <a href="<?php bloginfo('url') ?>"><br><button class="btn-info">Retourner sur la page d'accueil</button></a>
+                    <a href="<?php bloginfo('url') ?>"><br><button class="btn-danger">Retourner sur la page d'accueil</button></a>
                 </div>
 
             </div>
