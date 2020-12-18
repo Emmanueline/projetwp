29d806deec0d4b65c0b771b8b1cf74b28683f42d
Teddysa <emilie.lefebvre1999@gmail.com>
2020-12-18 10:38:55 +0100

    changement emplacement banner + ajout dans category

---
+++ category-advancedanalytics.php
@@ -0,0 +1,6 @@
+<?php get_header();?>
+
+<!-- Hero -->
+<header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png">
+    <p class="text-md-left">Left aligned text on viewports sized MD (medium) or wider.</p>
+</header>
---
+++ category-bigdata.php
@@ -0,0 +1,4 @@
+<?php get_header();?>
+
+<!-- Hero -->
+<header class="py-6 bg-image-full banner" style="background-image: url('https://cvanderc.chef2projet.digital/www/wordpress/wp-content/uploads/2020/12/iot.png"></header>
